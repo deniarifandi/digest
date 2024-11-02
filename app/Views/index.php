2a68f63cb896178598b81fcd4f2f850cbc545c43
deniarifandi <arifandi.malang@gmail.com>
2024-11-02 11:06:03 +0700

    1

---
+++ app/Views/index.php
@@ -0,0 +1,43 @@
+   
+    <?php include('landingComponent/1_header.php');?>
+  <main class="main">
+
+    <!-- Hero Section -->
+   
+    <?php include('landingComponent/2_hero.php');?>
+
+    <!-- Services Section -->
+    <?php include('landingComponent/3_service.php');?>
+    <!-- /Services Section -->
+
+    <!-- About Section -->
+     <?php include('landingComponent/4_about.php');?>
+    <!-- /About Section -->
+
+    <!-- featured Section -->
+    <?php include('landingComponent/5_featured.php');?>
+    <!-- /featured Section -->
+
+    <!-- Call To Action Section -->
+    <?php include('landingComponent/6_call.php');?>
+    <!-- /Call To Action Section -->
+
+    <!-- Features Section -->
+    <?php include('landingComponent/7_feature.php');?>
+    <!-- /Features Section -->
+
+    <!-- Pricing Section -->
+    <?php include('landingComponent/8_pricing.php');?>
+    <!-- /Pricing Section -->
+
+    <!-- Testimonials Section -->
+    <?php include('landingComponent/9_testimonial.php');?>
+    <!-- /Testimonials Section -->
+
+    <!-- Faq Section -->
+    <?php include('landingComponent/10_faq.php');?>
+    <!-- /Faq Section -->
+
+  </main>
+
+<?php include('landingComponent/footer.php');?>
