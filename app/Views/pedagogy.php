3850aec3b6633719cc297cf6e0ad7750dbf1891b
deniarifandi <42629835+deniarifandi@users.noreply.github.com>
2025-03-24 05:55:34 +0700

    Update pedagogy.php

--- app/Views/pedagogy.php
+++ app/Views/pedagogy.php
@@ -51,6 +51,36 @@
                      <td><a href="https://drive.google.com/file/d/1ATgGdVuxAe5KQ_Wp4Bgy0SGWfTB-PPe4/view?usp=sharing" target="_blank">View</a></td>
                   </tr>
                <?php endif ?>
+
+               <?php if ($pedagogys[0]->subject_id == 4): ?>
+                  <tr>
+                     <td>1</td>
+                     <td>Introduction to Bio-Chemistry</td>
+                     <td><a href="https://drive.google.com/file/d/138gbS5XmHn64GhLZiUmODLkTEwPXg8C7/view?usp=sharing" target="_blank">View</a></td>
+                  </tr>
+               <?php endif ?>
+
+               <?php if ($pedagogys[0]->subject_id == 7): ?>
+                  <tr>
+                     <td>1</td>
+                     <td>Comparison of Government Systems Worldwide</td>
+                     <td><a href="https://drive.google.com/file/d/1mFiQpk5NvK6sl_IAY-J_94aAsQTHyaL9/view?usp=sharing" target="_blank">View</a></td>
+                  </tr>
+                   <tr>
+                     <td>2</td>
+                     <td>Desc : Social Humaniora - Japanese Government</td>
+                     <td><a href="https://drive.google.com/file/d/1sECfsSMLDnUYTWgj9RY6W6QzdtnQ5eO3/view?usp=sharing" target="_blank">View</a></td>
+                  </tr>
+               <?php endif ?>
+
+               <?php if ($pedagogys[0]->subject_id == 11): ?>
+                  <tr>
+                     <td>1</td>
+                     <td>deliver bad news to an employee</td>
+                     <td><a href="https://drive.google.com/file/d/11tBKOOlp98AL4CO11gzhuTpEYA4UVoPx/view?usp=sharing" target="_blank">View</a></td>
+                  </tr>
+                  
+               <?php endif ?>
                   
                <?php foreach ($pedagogys as $pedagogy): ?>
                   <tr>
